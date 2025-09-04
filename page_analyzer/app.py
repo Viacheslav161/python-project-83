98015e40e77b22536c138ed8b4cafffd37983a69
Viacheslav <shtirlizz@yandex.ru>
2025-09-04 19:48:35 +0300

    fix 1st step

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,22 @@
 import os
-from flask import Flask
+
+import requests
 from dotenv import load_dotenv
+from flask import (
+    Flask,
+    abort,
+    flash,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 
 load_dotenv()
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev")
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 @app.route("/")
 def index():
