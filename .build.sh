5b8f28d78ec0fc6c85a42b66c0871740b66c2c2b
Viacheslav <shtirlizz@yandex.ru>
2025-09-08 20:02:16 +0300

    2 step

--- .build.sh
+++
@@ -1,3 +0,0 @@
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-make install
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,23 +1,12 @@
+from flask import Flask, render_template
 import os
-
-import requests
 from dotenv import load_dotenv
-from flask import (
-    Flask,
-    abort,
-    flash,
-    redirect,
-    render_template,
-    request,
-    url_for,
-)
 
 load_dotenv()
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-DATABASE_URL = os.getenv('DATABASE_URL')
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello, Hexlet!"
+    return render_template('index.html')
