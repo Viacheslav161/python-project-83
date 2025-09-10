1e06803d6083f38b5b4f2420032c4563af926856
Viacheslav <shtirlizz@yandex.ru>
2025-09-10 17:35:48 +0300

    2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,8 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# Поддержка SECRET_KEY через .env или дефолт для разработки
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev_secret')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    # В разработке удобно debug=True, в проде уберите
+    app.run(debug=True)
