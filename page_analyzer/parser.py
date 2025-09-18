41f42afb4b1c40f3b9b53e4eb9a1774c755f4073
Viacheslav <shtirlizz@yandex.ru>
2025-09-18 18:14:52 +0300

    changed parser

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -5,13 +5,13 @@ def get_data(response):
     parsed_content = BeautifulSoup(response.text, "lxml")
     result = {}
 
-    heading = parsed_content.h1.string if parsed_content.h1 else None
-    page_title = parsed_content.title.string if parsed_content.title else None
+    heading = parsed_content.h1.string if parsed_content.h1 else ""
+    page_title = parsed_content.title.string if parsed_content.title else ""
     meta_description = parsed_content.find('meta', {'name': 'description'})
 
     result['h1'] = heading
     result['title'] = page_title
-    result['description'] = meta_description.get('content') \
-        if meta_description else None
+    result['description'] = meta_description.get('content') if meta_description else ""
 
     return result
+
