840c633fc55a1a7f34528cb10305b9648afc958d
Viacheslav <shtirlizz@yandex.ru>
2025-09-18 20:29:36 +0300

    1

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -1,20 +1,18 @@
 from bs4 import BeautifulSoup
 
-
 def get_data(response):
     parsed_content = BeautifulSoup(response.text, "lxml")
     result = {}
 
     result['h1'] = parsed_content.h1.string if parsed_content.h1 else ""
-    result['title'] = parsed_content.title.string if parsed_content.title else ""
-    meta_description = parsed_content.find(
-        'meta', {'name': 'description'}
+    result['title'] = (
+        parsed_content.title.string if parsed_content.title else ""
     )
-
+    
+    meta_description = parsed_content.find('meta', {'name': 'description'})
+    
     result['description'] = (
-        meta_description.get('content')
-        if meta_description
-        else ""
+        meta_description.get('content') if meta_description else ""
     )
 
     return result
